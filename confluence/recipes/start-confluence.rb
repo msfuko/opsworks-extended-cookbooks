e9263ae87881bff896fa624fef44c3adb7de334e
齋藤秀人 <saito@Hideto-Saito-no-MacBook-Pro.local>
2014-08-28 12:00:41 -0700

    add start script

--- confluence/recipes/start-confluence.rb
+++ confluence/recipes/start-confluence.rb
@@ -1,10 +1,3 @@
-user "confluence" do
-  uid 2001
-  gid 2001
-  home "/home/confluence"
-  shell "/bin/bash"
-end
-
 execute "start-confluence" do
   command "/trend/atlassian/confluence/bin/start-confluence.sh"
   action :run
