8f54fc206e86917f9a67365a95668dcdd93bed49
齋藤秀人 <saito@Hideto-Saito-no-MacBook-Pro.local>
2014-08-28 12:13:42 -0700

    adjust

--- confluence/recipes/default.rb
+++ confluence/recipes/default.rb
@@ -1,11 +1,6 @@
 user "confluence" do
   uid 2001
-  gid 2001
+#  gid 2001
   home "/home/confluence"
   shell "/bin/bash"
-end
-
-execute "start-confluence" do
-  command "/trend/atlassian/confluence/bin/start-confluence.sh"
-  action :run
 end
--- confluence/recipes/start-confluence.rb
+++ confluence/recipes/start-confluence.rb
@@ -1,4 +1,6 @@
 execute "start-confluence" do
   command "/trend/atlassian/confluence/bin/start-confluence.sh"
+  user "root"
+  cwd "/"
   action :run
 end
