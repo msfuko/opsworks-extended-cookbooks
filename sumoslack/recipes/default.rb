a78d7daf31ed54dfb7db5d8c7de0a4943c4503d0
Chloe Lee <chloe_lee@trend.com.tw>
2014-12-31 16:44:04 +0800

    fix the path

--- sumoslack/recipes/default.rb
+++ sumoslack/recipes/default.rb
@@ -1,11 +1,13 @@
+scripts = node["sumoslack"][:scripts]
+
 # install scripts
-template "#{node["sumoslack"][:scripts][:install_dir]}/slack.sh" do
+template "#{scripts[:install_dir]}/slack.sh" do
   source "slack.sh.erb"
   owner scripts[:user]
   group scripts[:group]
   mode '755'
   variables({
                 :webhook => node["sumoslack"]["webhook"],
-                :alertfolder => node["sumoslack"][:scripts][:alert_dir]
+                :alertfolder => scripts[:alert_dir]
             })
 end
