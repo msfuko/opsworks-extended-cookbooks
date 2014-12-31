dd359735eeda7461ee1f9321b767538619f14e5e
Chloe Lee <chloe_lee@trend.com.tw>
2014-12-31 17:18:59 +0800

    ....change .sh to .py

--- sumoslack/recipes/default.rb
+++ sumoslack/recipes/default.rb
@@ -1,8 +1,8 @@
 scripts = node["sumoslack"][:scripts]
 
 # install scripts
-template "#{scripts[:install_dir]}/slack.sh" do
-  source "slack.sh.erb"
+template "#{scripts[:install_dir]}/slack.py" do
+  source "slack.py.erb"
   owner scripts[:user]
   group scripts[:group]
   mode '755'
