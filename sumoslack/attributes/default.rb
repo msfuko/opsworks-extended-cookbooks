7100b898f2f0b5abb59ec7910fb377e6a48473b7
Chloe Lee <chloe_lee@trend.com.tw>
2014-12-31 16:13:00 +0800

    add sumoslack

---
+++ sumoslack/attributes/default.rb
@@ -0,0 +1,7 @@
+default["sumoslack"] = {}
+
+default["sumoslack"]["scripts"] = {}
+default["sumoslack"]["scripts"]["user"] = "root"
+default["sumoslack"]["scripts"]["group"] = "root"
+default["sumoslack"]["scripts"]["install_dir"] = "/opt/SumoCollector"
+default["sumoslack"]["scripts"]["alert_dir"] = "./alerts"
---
+++ sumoslack/recipes/default.rb
@@ -0,0 +1,11 @@
+# install scripts
+template "#{scripts[:install_dir]}/slack.sh" do
+  source "slack.sh.erb"
+  owner scripts[:user]
+  group scripts[:group]
+  mode '755'
+  variables({
+                :webhook => node["sumoslack"]["webhook"],
+                :alertfolder => node["sumoslack"][:alert_dir]
+            })
+end
