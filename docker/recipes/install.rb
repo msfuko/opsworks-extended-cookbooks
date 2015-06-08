4ef521041c0fbff27d340b30ddf490546403c52e
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-06-08 19:46:45 +0800

    docker update

--- docker/recipes/install.rb
+++ docker/recipes/install.rb
@@ -7,6 +7,11 @@ when 'centos','redhat','fedora','amazon'
   package "docker" do
     action :install
   end
+  remote_file "/usr/bin/docker" do
+	source "https://get.docker.com/builds/Linux/x86_64/docker-latest"
+	mode '0777'
+	notifies :run, "service[docker]", :immediately
+  end
 end
 
 service "docker" do
