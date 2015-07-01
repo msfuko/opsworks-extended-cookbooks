9670b7406eda398d6f3d1953edc148932395bf6e
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-07-01 17:27:58 +0800

    remove upgrade docker

--- docker/recipes/install.rb
+++ docker/recipes/install.rb
@@ -7,14 +7,14 @@ when 'centos','redhat','fedora','amazon'
   package "docker" do
     action :install
   end
-  bash "upgrate_docker" do
-	user "root"
-	code <<-EOH
-		mv /usr/bin/docker /usr/bin/docker-old
-		wget https://get.docker.com/builds/Linux/x86_64/docker-latest -O /usr/bin/docker
-		chmod 777 /usr/bin/docker
-	EOH
-  end
+  #bash "upgrate_docker" do
+	#user "root"
+	#code <<-EOH
+	#	mv /usr/bin/docker /usr/bin/docker-old
+	#	wget https://get.docker.com/builds/Linux/x86_64/docker-latest -O /usr/bin/docker
+	#	chmod 777 /usr/bin/docker
+	#EOH
+  #end
 end
 
 service "docker" do
