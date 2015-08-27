313c0bae13bedbca390d9e4c73f6850bdd899ef1
Chloe Lee (DCS-RD-TW) <chloe_lee@trend.com.tw>
2015-08-27 16:03:08 +0800

    add default value

---
+++ docker/attibutes/default.rb
@@ -0,0 +1,8 @@
+# docker registry url
+default['docker']['registry'] = ""
+
+# docker registry auth base64
+default['docker']['auth'] = ""
+
+# docker registry auth email
+default['docker']['email'] = "dcsrd@trendmicro.com"
